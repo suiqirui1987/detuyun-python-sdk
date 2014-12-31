c16be53111144754576b09f7b55f1e7458923505
孙其瑞 <sqr@detu.com>
2014-12-31 09:55:37 +0800

    update

---
+++ detuyun/__init__.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__title__ = 'detuyun'
+__version__ = '1.0.0'
+__author__ = ''
+__license__ = 'MIT License: http://www.opensource.org/licenses/mit-license.php'
+__copyright__ = 'Copyright 2013 DetuYun.com'
+
+from .detuyun import DetuYun, DetuYunServiceException, DetuYunClientException
